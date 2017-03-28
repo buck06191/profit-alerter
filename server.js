87bf4fd44e0e3ab31259cabf406abfa41d91c1bc
Josh Buckland <buck06191@gmail.com>
2017-03-28 14:15:17 +0100

    include server side js code

--- server.js
+++ server.js
@@ -1,21 +1,52 @@
-var express = require('express');
-var app = express();
+/*jshint esversion: 6 */
+'use strict';
 
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const request = require('request');
+const stream = require('stream');
+var API_DOMAIN = "https://api.kraken.com";
+var INTERVAL = 1; // Poll interval in seconds.
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
+
+
+const server = express()
+    .get('/', (req, res) => res.render('index'))
+    .set('view engine', 'ejs')
+    .use(express.static(__dirname + '/public'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 // set the view engine to ejs
-app.set('view engine', 'ejs');
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+
+
+// make express look in the public directory for assets (css/js/img
 // set the home page route
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     // ejs render automatically looks in the views folder
     res.render('index');
+});*/
+
+
+const io = socketIO(server);
+
+io.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+setInterval(() => {
+    var url = API_DOMAIN;
+    var timeURL = url + "/0/public/Time";
+    var time = new stream.Writable();
+    request(timeURL, function(err, response, body) {
+        if (err) {
+            console.log(err);
+        } else {
+            io.emit('time', body);
+        }
+    });
+}, INTERVAL * 1000);
