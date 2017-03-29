639713230abc7dc9c327fe1f9a56d6c25c86e336
Josh Buckland <buck06191@gmail.com>
2017-03-29 11:10:57 +0100

    include api polling for ticker and pairs

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 var socket = io();
 console.log(socket);
-var el = document.getElementById('server-time');
+var server_time = document.getElementById('server-time');
 
 socket.on('time', function(object) {
   var timeString = JSON.parse(object);
-    el.innerHTML = 'Server time: ' + timeString.result.rfc1123;
+  server_time.innerHTML = 'Server time: ' + timeString.result.rfc1123;
 });
--- server.js
+++ server.js
@@ -7,28 +7,20 @@ const path = require('path');
 const request = require('request');
 const stream = require('stream');
 var API_DOMAIN = "https://api.kraken.com";
-var INTERVAL = 1; // Poll interval in seconds.
+var INTERVAL = 10; // Poll interval in seconds.
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 3000;
 
 
-
+// set the view engine to ejs
+// make express look in the public directory for assets (css/js/img
+// set the home page route
 const server = express()
     .get('/', (req, res) => res.render('index'))
     .set('view engine', 'ejs')
     .use(express.static(__dirname + '/public'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-// set the view engine to ejs
-
-
-
-// make express look in the public directory for assets (css/js/img
-// set the home page route
-/*app.get('/', function(req, res) {
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});*/
 
 
 const io = socketIO(server);
@@ -38,15 +30,65 @@ io.on('connection', (ws) => {
     ws.on('close', () => console.log('Client disconnected'));
 });
 
-setInterval(() => {
-    var url = API_DOMAIN;
+// Define API data
+
+var api_data = {
+    "pair": ["ETHXBT", "ETHEUR", "XBTEUR"],
+};
+
+// Define API polling functions
+
+var getTime = function(url) {
     var timeURL = url + "/0/public/Time";
-    var time = new stream.Writable();
-    request(timeURL, function(err, response, body) {
+    request({
+        method: "GET",
+        url: timeURL
+    }, function(err, response, body) {
         if (err) {
             console.log(err);
         } else {
             io.emit('time', body);
         }
     });
+};
+
+
+var getPairs = function(url, data) {
+    var pairsURL = url + "/0/public/AssetPairs";
+    request({
+      method: "GET",
+      uri: pairsURL,
+      qs: api_data
+    }, function(err, response, body) {
+      if (err){
+        console.error(err);
+      } else{
+        console.log("Response: " + response);
+        console.log("Body: " + body);
+      }
+    });
+};
+
+var getTicker = function(url, data) {
+    var tickerURL = url + "/0/public/Ticker";
+    request({
+      method: "GET",
+      uri: tickerURL,
+      qs: api_data
+    }, function(err, response, body) {
+      if (err){
+        console.error(err);
+      } else{
+        console.log("Response: " + response);
+        console.log("Body: " + body);
+      }
+    });
+};
+};
+
+setInterval(() => {
+    var url = API_DOMAIN;
+    getTime(url);
+    getPairs(url);
+    getTicker(url);
 }, INTERVAL * 1000);
