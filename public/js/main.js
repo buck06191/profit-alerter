872987233f1e6a74646939c42dbb4bac35907c42
Josh Buckland <buck06191@gmail.com>
2017-03-29 11:41:21 +0100

    show all data on client side

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 var socket = io();
 console.log(socket);
 var server_time = document.getElementById('server-time');
-
+var pair_data = document.getElementById('pair-data');
+var ticker_data = document.getElementById('ticker-data');
 socket.on('time', function(object) {
   var timeString = JSON.parse(object);
   server_time.innerHTML = 'Server time: ' + timeString.result.rfc1123;
@@ -10,11 +11,11 @@ socket.on('time', function(object) {
 socket.on('pair', function(object) {
   var pairString = JSON.parse(object);
   console.log(pairString);
-  server_time.innerHTML = 'Server time: ' + pairString;
+  pair_data.innerHTML = 'Pair Data: ' + pairString;
 });
 
 socket.on('ticker', function(object) {
   var tickerString = JSON.parse(object);
   console.log(tickerString);
-  server_time.innerHTML = 'Server time: ' + tickerString;
+  ticker_data.innerHTML = 'Ticker Data: ' + tickerString;
 });
